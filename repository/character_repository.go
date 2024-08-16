6c901b39926b57533c71a40bb24028b067aeb79f
HilbertGreveling <HilbertGreveling@users.noreply.github.com>
2024-08-16 16:29:16 +0200

    Moved functions so it follows CRUD

--- repository/character_repository.go
+++ repository/character_repository.go
@@ -3,9 +3,9 @@ package repository
 import "github.com/hilbertgreveling/dnd-character-api/models"
 
 type CharacterRepository interface {
+	Create(character *models.Character) error
 	GetAll() ([]*models.Character, error)
 	GetByID(id int) (*models.Character, error)
-	Create(character *models.Character) error
 	Update(character *models.Character) error
 	Delete(id int) error
 }
